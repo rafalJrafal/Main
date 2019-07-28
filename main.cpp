fa9eb059c0104e3c3afdb26a522d5553babab9ed
Raf <rafal.jakuta@gmail.com>
2019-07-28 13:54:21 +0200

    Added DEV project

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "LogSystem/inc/LogSystem.h"
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	LogSystem::LogSystem l;
+	l.log("aaa");
+	return 0;
+}
