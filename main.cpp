1dbddb7cc887dd1e12719955059e0b8857900d34
Raf <rafal.jakuta@gmail.com>
2019-07-31 19:19:00 +0200

    added missing cpp file

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 
 #include "LogSystem/inc/LogSystem.h"
-
+#include "ParameterFile/inc/ParameterFile.h"
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
 	LogSystem::LogSystem l;
 	l.log("aaa");
+	
+	Parameter::Parameter p("param.txt");
+	p.readParameters();
 	return 0;
 }
