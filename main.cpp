dd683531145719d564ca0896a82e4bdffecc2f7c
Raf <rafal.jakuta@gmail.com>
2019-08-01 13:58:16 +0200

    example conversion added

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 #include "LogSystem/inc/LogSystem.h"
 #include "ParameterFile/inc/ParameterFile.h"
+#include "ParameterFile/inc/ParameterFileDataType.h"
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
@@ -10,6 +11,8 @@ int main(int argc, char** argv) {
 	
 	Parameter::Parameter p("param.txt");
 	p.readParameters();
-	p.getValue("exampleStringValue");
+	ParemeterFileData<string> * param = static_cast<ParemeterFileData<string> *>(p.getValue("exampleStringValue"));
+	
+	printf("----> %s <----\n", param->getValue().c_str());
 	return 0;
 }
