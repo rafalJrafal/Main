da1761507dfa7e1d38421dddf723280f98611789
Raf <rafal.jakuta@gmail.com>
2019-08-08 14:22:09 +0200

    log system changes

--- main.cpp
+++ main.cpp
@@ -3,16 +3,23 @@
 #include "LogSystem/inc/LogSystem.h"
 #include "ParameterFile/inc/ParameterFile.h"
 #include "ParameterFile/inc/ParameterFileDataType.h"
+#include "Operator/inc/ClassX.h"
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	LogSystem::LogSystem l;
-	l.log("aaa");
+	LogSystem::LogSystem * l = LogSystem::LogSystem::instance();
+	l->log("aaa");
 	
 	Parameter::Parameter p("param.txt");
 	p.readParameters();
 	
 	int x = p.getVal<int>("exampleIntValue");
 	printf("----> %d <----\n", x);
+	
+	OperatorTest::ClassX a, b;
+	a = 3;
+	
+	//a = b;
+	
 	return 0;
 }
