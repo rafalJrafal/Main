5985c334efab10dcadd4818586cdeb2ca8138d0f
Raf <rafal.jakuta@gmail.com>
2019-08-22 21:58:30 +0200

    memory monitor submodule changes

--- main.cpp
+++ main.cpp
@@ -2,17 +2,24 @@
 
 #include "SystemCalls/inc/Memory.h"
 
+#define new new(__FILE__, __LINE__)
+
+
 #include "SystemCalls/inc/MemoryMap.h"
 #include "LogSystem/inc/LogSystem.h"
 #include "ParameterFile/inc/ParameterFile.h"
 #include "ParameterFile/inc/ParameterFileDataType.h"
 #include "Operator/inc/ClassX.h"
+
+
+
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-
+	{
+	//memoryMap.setPrintOtherDeallocations(false);
 	int * xxczvd = new int;
-	delete 	xxczvd;
+	delete xxczvd;
 	
 	int * asd = new int [300];
 	delete [] asd;
@@ -22,29 +29,30 @@ int main(int argc, char** argv) {
 	l->log("aaa");
 	
 	Parameter::Parameter p("param.txt");
-	p.readParameters();
+	//p.readParameters();
 	
 	int x = p.getVal<int>("exampleIntValue");
 	printf("----> %d <----\n", x);
 	
 	OperatorTest::ClassX a, b, c, d, e, f, g, h;
 	
-	OperatorTest::ClassX * a_p = new OperatorTest::ClassX;
-	OperatorTest::ClassX * a2_p = new (10) OperatorTest::ClassX;
+/*	OperatorTest::ClassX * a_p = new OperatorTest::ClassX();
+	//OperatorTest::ClassX * a2_p = new (10) OperatorTest::ClassX;
 	OperatorTest::ClassX * b_p = new OperatorTest::ClassX [2];
-	OperatorTest::ClassX * c_p = new OperatorTest::ClassX [3];
+	OperatorTest::ClassX * c_p = new OperatorTest::ClassX [3]; */
 	
-	delete a_p, a2_p;
-	delete [] b_p;
-	delete [] c_p;
+	//delete a_p;
+	//delete a2_p;
+	//delete [] b_p;
+	//delete [] c_p;
 	
 	a = 3;
 	b = a;
 	c = a + b;
-	
-	static MemoryMap & memoryMap = MemoryMap::instance();
+    }
+   	static MemoryMap & memoryMap = MemoryMap::instance();
 	memoryMap.printRemainingAllocations();
-	printf("----> %d <----\n", x);
+
 
 	return 0;
 }
